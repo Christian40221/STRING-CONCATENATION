7b93768e294698f5db38ce8e08ebcf46b521f2fa
Christian40221 <christiansabido402@gmail.com>
2024-11-19 16:40:35 +0800

    Update public class StringConcantenation{.java

--- public class StringConcantenation{.java
+++ public class StringConcantenation{.java
@@ -1,43 +1,38 @@
-public class StringConcantenation{
-    public static String concatenatedStrings(String str1, String str2, String str3){
+public class StringConcatenation {
+    public static String concatenatedStrings(String str1, String str2, String str3) {
         int len1 = str1.length();
         int len2 = str2.length();
         int len3 = str3.length();
 
-        char [] str4 = new char{len1 + len2 + len3};
+        char[] str4 = new char[len1 + len2 + len3];
 
         int i = 0;
-        while (i < len1){
-            str3[i] = str1.charAt(j);
+        while (i < len1) {
+            str4[i] = str1.charAt(i);
             i++;
         }
         int j = 0;
-        while (j < len1+len2){
-            str3[i] = str2.charAt(j);
+        while (j < len2) {
+            str4[i] = str2.charAt(j);
             i++;
             j++;
-        }
-        int k = 0;
-        while (k < len1 + len2 + len3){
-            str3 [i] = str3.charAt(k);
-            i++;
-            j++;
-            k++
         }
 
+        int k = 0;
+        while (k < len3) {
+            str4[i] = str3.charAt(k);
+            i++;
+            k++;
+        }
         return new String(str4);
     }
-    
-    public static void main(String[] args){
-        String str1 = "Christian";
-        String str2 = "Chavez";
-        String str3 = "Sabido";
+
+    public static void main(String[] args) {
+        String str1 = "Christian ";
+        String str2 = ",Chavez ";
+        String str3 = ",Sabjdo";
 
         String concatenatedString = concatenatedStrings(str1, str2, str3);
         System.out.println("Concatenated String: " + concatenatedString);
-        
-
     }
-
-
 }
